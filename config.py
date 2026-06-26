d7b7818dd6a3f951fe2c597d51f58183635ffbd4
Abdullah Ameen-Ikoyi <olagbabs@gmail.com>
2026-06-26 12:40:32 +0100

    Add file categories for media types in config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+FILE_CATEGORIES = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp", ".svg", ".heic", ".tiff"],
+    "Documents": [".pdf", ".docx", ".txt", ".xlsx", ".pptx", ".csv", ".doc", ".xls", ".odt"],
+    "Videos": [".mp4", ".mkv", ".mov", ".avi", ".webm", ".wmv"],
+    "Audio": [".mp3", ".wav", ".aac", ".flac"],
+}
